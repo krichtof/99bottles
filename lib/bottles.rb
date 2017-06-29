88a11fd92e4a752ab2421a9e0060c9150ec33794
krichtof <geek@robiweb.net>
2017-06-29 12:50:32 +0200

    implement solution

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,55 @@
 class Bottles
+  def song
+    verses(99,0)
+  end
+
+  def verses(starting, ending)
+    starting.downto(ending).map{|number| verse(number)}.join("\n")
+  end
+
   def verse(number)
-    "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
-    "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n" +
+    "#{action(number)}, " +
+      "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+  end
+
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def pronoun(number)
+    if number == 0
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number-1)} down and pass it around"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
   end
 end
